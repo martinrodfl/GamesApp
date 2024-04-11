fba692f50aae6e54eb5ad33c9c178cd1b3ac5828
martinrodfl <martinrodfl@gmail.com>
2024-04-11 15:21:53 -0300

    Pre-first-deploy

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,86 +1,86 @@
-import { useEffect, useState, memo } from 'react';
-import { Card } from './Card.jsx';
-import './CardList.css';
+import { useEffect, useState, memo } from "react";
+import { Card } from "./Card.jsx";
+import "./CardList.css";
 
 export const CardList = memo(() => {
-	const [items, setItems] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState(null);
-	const [page, setPage] = useState(1);
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
 
-	const fetchData = async () => {
-		setIsLoading(true);
+  const fetchData = async () => {
+    setIsLoading(true);
 
-		setError(null);
+    setError(null);
 
-		try {
-			if (page > 10) {
-				return;
-			}
+    try {
+      if (page > 10) {
+        return;
+      }
 
-			const response = await fetch(
-				`https://rawg.io/api/games/lists/popular?discover=
+      const response = await fetch(
+        `https://rawg.io/api/games/lists/popular?discover=
 				true&&page_size=25&page=${page}&key=01de356b02bb4257b02f2c2ca4c78ad6`
-			);
-			const data = await response?.json();
-			// console.log(data);
-			if (page === 1) {
-				setItems(data?.results);
-			} else {
-				setItems((prevItems) => [...prevItems, ...data?.results]);
-			}
+      );
+      const data = await response?.json();
+      // console.log(data);
+      if (page === 1) {
+        setItems(data?.results);
+      } else {
+        setItems((prevItems) => [...prevItems, ...data?.results]);
+      }
 
-			setPage(page + 1);
-		} catch (error) {
-			setError(error);
-		} finally {
-			setIsLoading(false);
-		}
-	};
+      setPage(page + 1);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-	useEffect(() => {
-		window.scrollTo(0, 0);
-		fetchData();
-	}, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    fetchData();
+  }, []);
 
-	const handleScroll = () => {
-		if (
-			window.innerHeight + document.documentElement.scrollTop !==
-				document.documentElement.offsetHeight ||
-			isLoading
-		) {
-			return;
-		}
-		fetchData();
-	};
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight ||
+      isLoading
+    ) {
+      return;
+    }
+    fetchData();
+  };
 
-	useEffect(() => {
-		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, [isLoading]);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [isLoading]);
 
-	return (
-		<div className='cardList-body'>
-			<div className='game-list'>
-				<>
-					{Array.isArray(items) &&
-						items.map((item, index) => (
-							<Card
-								key={item.id}
-								id={item.id}
-								index={index}
-								slug={item.name}
-								image={item.background_image}
-							/>
-						))}
-				</>
-			</div>
-			{isLoading && (
-				<div className='loadership_ELDPB'>
-					<div></div>
-					<div></div>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div className="cardList-body">
+      <div className="game-list">
+        <>
+          {Array.isArray(items) &&
+            items.map((item, index) => (
+              <Card
+                key={item.id}
+                id={item.id}
+                index={index}
+                slug={item.name}
+                image={item.background_image}
+              />
+            ))}
+        </>
+      </div>
+      {isLoading && (
+        <div className="loadership_ELDPB">
+          <div></div>
+          <div></div>
+        </div>
+      )}
+    </div>
+  );
 });
