4dfb1a406be66c2b5e27cc117fce4f30907f7e83
martinrodfl <martinrodfl@gmail.com>
2024-04-14 21:39:09 -0300

    Fixed css and change endpoints

--- src/helpers/getInitials.js
+++ src/helpers/getInitials.js
@@ -1,5 +1,5 @@
 export const getInitials = (str) => {
-	const words = str.split(' ');
-	const initials = words.map((word) => word.charAt(0).toUpperCase());
-	return initials.join('');
+  const words = str.split(" ");
+  const initials = words.map((word) => word.charAt(0).toUpperCase());
+  return initials.join("");
 };
